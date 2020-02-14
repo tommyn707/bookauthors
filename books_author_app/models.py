575eca4b6a3f2d4b6c2c116e2480ffcd748b6b88
tommyn707 <tommy.n707@gmail.com>
2020-02-14 14:56:26 -0800

    first commit

---
+++ books_author_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class authors(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+class books(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    authors = models.ForeignKey(authors, related_name="books", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
